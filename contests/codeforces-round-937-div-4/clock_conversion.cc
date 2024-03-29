77bcbe01a2b28c720695abd955d7c6652e1f8641
Kleber Rodrigues <kleber.junior@pushstart.com.br>
2024-03-29 13:29:32 -0300

    clock_conversion problem upsolving

--- contests/codeforces-round-937-div-4/clock_conversion.cc
+++ contests/codeforces-round-937-div-4/clock_conversion.cc
@@ -3,46 +3,57 @@
 
 using namespace std;
 
-int main() {
-  lli t;
-  cin >> t;
+void upsolving() {
+  int h, m;
+  char c;
 
+  // Like this it's possible to read an input as 00:00 and separate it in different variables
+  cin >> h >> c >> m;
+
+  // there is no great solution here, mine is fine
+}
+
+void first_solution() {
   string timestamp;
   int hours, minutes;
+  cin >> timestamp;
 
-  char *s;
-  for (lli i = 0; i < t; i++) {
-    cin >> timestamp;
+  hours = stoi(timestamp);
+  
+  if (hours == 0) {
+    cout << 12 << ":" << timestamp[3] << timestamp[4] << " AM" << endl;
+    return;
+  }
 
-    hours = stoi(timestamp);
-    
-    if (hours == 0) {
-      cout << 12 << ":" << timestamp[3] << timestamp[4] << " AM" << endl;
-      continue;
+  if (hours == 12) {
+    cout << 12 << ":" << timestamp[3] << timestamp[4] << " PM" << endl;
+    return;
+  }
+
+  if (hours > 12) {
+    if (hours % 12 < 10) {
+      cout << "0" << hours % 12 << ":" << timestamp[3] << timestamp[4] << " PM" << endl;
+      return;
     }
 
-    if (hours == 12) {
-      cout << 12 << ":" << timestamp[3] << timestamp[4] << " PM" << endl;
-      continue;
+    cout << hours % 12 << ":" << timestamp[3] << timestamp[4] << " PM" << endl;
+  } 
+
+  if (hours < 12) {
+    if (hours < 10) {
+      cout << "0" << hours % 12 << ":" << timestamp[3] << timestamp[4] << " AM" << endl;
+      return;
     }
 
-    if (hours > 12) {
-      if (hours % 12 < 10) {
-        cout << "0" << hours % 12 << ":" << timestamp[3] << timestamp[4] << " PM" << endl;
-        continue;
-      }
+    cout << hours << ":" << timestamp[3] << timestamp[4] << " AM" << endl;
+  }
+}
 
-      cout << hours % 12 << ":" << timestamp[3] << timestamp[4] << " PM" << endl;
-    } 
+int main() {
+  lli t; cin >> t;
 
-    if (hours < 12) {
-      if (hours < 10) {
-        cout << "0" << hours % 12 << ":" << timestamp[3] << timestamp[4] << " AM" << endl;
-        continue;
-      }
-
-      cout << hours << ":" << timestamp[3] << timestamp[4] << " AM" << endl;
-    }
+  while(t--) {
+    upsolving();
   }
 
   return 0;
